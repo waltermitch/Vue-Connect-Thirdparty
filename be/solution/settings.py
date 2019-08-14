463f3538afbf8001f8ef597ad59118e5eceed9ff
waltermitch <devmitch256@gmail.com>
2019-08-14 21:29:38 +0100

    Postgres DB setup

--- be/solution/settings.py
+++ be/solution/settings.py
@@ -82,11 +82,22 @@ WSGI_APPLICATION = 'solution.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'favorite_db',
+        'USER': 'favorite_db_user',
+        'PASSWORD': 'favorite@db!user',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
