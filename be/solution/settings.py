676ac6e66df14f59e63596646870215430cf29b3
waltermitch <devmitch256@gmail.com>
2019-08-14 21:41:06 +0100

    setting static_url

--- be/solution/settings.py
+++ be/solution/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
